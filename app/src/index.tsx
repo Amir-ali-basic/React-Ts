c3fef32521c40b977cf76653ca21492941045540
Amir-ali-basic <alib.amir123@gmail.com>
2023-01-21 22:05:02 +0100

    Jest added

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import store from './store/store';
-import CharacterList from './componeents/CharacterList';
+import App from './App';
 
 ReactDOM.render(
   <Provider store={store}>
-    <CharacterList />
+    <App />
   </Provider>,
   document.getElementById('root')
 );
---
+++ app/src/test/viewModels/viewModel.test.ts
@@ -0,0 +1,37 @@
+import { CharacterModel } from "../../models/CharactersModel";
+import { CharacterViewModel } from "../../viewModels/CharactrsVm";
+
+describe('CharacterViewModel', () => {
+    let characters:any;
+    let viewModel:any;
+  
+    beforeEach(() => {
+      characters = [
+        { id: 1, name: 'Rick', status: 'Alive' },
+        { id: 2, name: 'Morty', status: 'Alive' },
+        { id: 3, name: 'Summer', status: 'Alive' },
+        { id: 4, name: 'Beth', status: 'Dead' },
+        { id: 5, name: 'Jerry', status: 'Dead' },
+      ];
+      viewModel = new CharacterViewModel(characters);
+    });
+  
+    it('should set allCharacters to the characters passed in the constructor', () => {
+      expect(viewModel.allCharacters).toEqual(characters);
+    });
+  
+    it('should set aliveCharacters to characters with status "Alive"', () => {
+      expect(viewModel.aliveCharacters).toEqual([
+        { id: 1, name: 'Rick', status: 'Alive' },
+        { id: 2, name: 'Morty', status: 'Alive' },
+        { id: 3, name: 'Summer', status: 'Alive' },
+      ]);
+    });
+  
+    it('should set deadCharacters to characters with status "Dead"', () => {
+      expect(viewModel.deadCharacters).toEqual([
+        { id: 4, name: 'Beth', status: 'Dead' },
+        { id: 5, name: 'Jerry', status: 'Dead' },
+      ]);
+    });
+});
