187a9749f6a501c2165dcd098397e1563cb442c5
Amir-ali-basic <alib.amir123@gmail.com>
2023-01-22 16:17:15 +0100

    Storybook added, need to export stories on another way to make docs better

---
+++ app/src/stories/test.stories.tsx
@@ -0,0 +1,44 @@
+import { ComponentStory } from "@storybook/react";
+import Character from "../componeents/Character";
+
+export default {
+  title: "Example/Character",
+  component: Character,
+  parameters: {
+    // More on Story layout: https://storybook.js.org/docs/react/configure/story-layout
+    layout: "fullscreen",
+  },
+};
+
+const Template: ComponentStory<typeof Character> = (args) => (
+  <Character {...args} />
+);
+export const Disabled = Template.bind({});
+Disabled.args = {
+  characters: [
+    {
+      id: 1,
+      name: "Rick",
+      status: "Alive",
+      species: "nesto",
+      gender: "druge",
+      image: "https://rickandmortyapi.com/api/character/avatar/1.jpeg",
+    },
+  ],
+  isDisabled: true,
+};
+
+export const Alive = Template.bind({});
+Alive.args = {
+  characters: [
+    {
+      id: 1,
+      name: "Rick",
+      status: "Alive",
+      species: "nesto",
+      gender: "druge",
+      image: "https://rickandmortyapi.com/api/character/avatar/1.jpeg",
+    },
+  ],
+  isDisabled: false,
+};
